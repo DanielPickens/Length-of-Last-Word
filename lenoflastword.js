20165c05f3d943a8077d459e26c85a80f023f714
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-03 15:35:46 -0600

    Update lenoflastword.js

--- lenoflastword.js
+++ lenoflastword.js
@@ -1,7 +1,7 @@
   const lengthOfLastWord = function(s) {
     s = s.trim();  //setting method of trim as property of s, where the trim() method removes whitespace from both ends of s 
     let count = 0;
-    for (let i = s.length-1; i>=0; i--){  //for loop will traverse the array of s  and if element i on the array's indice correlates, it will find correlation pointing to an open string dictionary, 
+    for (let i = s.length-1; i>=0; i--){  //for loop will traverse the array of s  and if element i on the array's indice correlates, it will find correlation enoguh to continue iteration, 
         if(s[i] === " " ){break;}
         
         else{count +=1;}  // setting constraint so until for loop has iterated across array and found value of 0 , traverse and return count value
